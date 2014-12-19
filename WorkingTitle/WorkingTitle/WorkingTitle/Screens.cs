ad6baf16986105cd80dbff6a4b27d5c1b02fa058
JBizzleDrizzle <jbanderson@scu.edu>
2014-12-19 15:59:31 -0800

    finished all drawing in Main Menu

--- WorkingTitle/WorkingTitle/WorkingTitle/Screens.cs
+++ WorkingTitle/WorkingTitle/WorkingTitle/Screens.cs
@@ -16,22 +16,36 @@ namespace WorkingTitle
     {
         private Texture2D MenuBackground;
         private SpriteFont font;
+        private Vector2 initialDraw;
         private Vector2 drawPosition;
+        private readonly string[] CHOICES={"New Game","Load Game","Options","About","Exit"};
 
          public MainMenu(Game game) : base(game)
         {
             font = game.Content.Load<SpriteFont>("MenuFont");
             MenuBackground = game.Content.Load<Texture2D>("MenuBackground");
-            drawPosition = new Vector2(Game1.screenWidth/2, Game1.screenHeight*2/10);
+            initialDraw = new Vector2(Game1.screenWidth/2, Game1.screenHeight*2/10);
         }
 
          public override void Draw(GameTime gameTime)
          {
+             drawPosition = initialDraw;
+
              manager.sb.Begin();
-             string output1 = "New Game";
-             Vector2 output1Origin = font.MeasureString(output1) / 2;
-             manager.sb.DrawString(font, output1, drawPosition, Color.Black,
-                 0, output1Origin, 1.0f, SpriteEffects.None, 0.5f);
+             //code to draw background of Main Menu
+             //need a better background
+             manager.sb.Draw(MenuBackground, new Rectangle(0, 0, Game1.screenWidth, Game1.screenHeight), Color.White);
+
+
+             foreach(string output in CHOICES)
+             {
+                 Vector2 outputOrigin = font.MeasureString(output) / 2;
+                 manager.sb.DrawString(font, output, drawPosition, Color.Black,
+                     0, outputOrigin, 1.0f, SpriteEffects.None, 0.5f);
+                 drawPosition.Y += outputOrigin.Y * 2;
+             }
+
+
              manager.sb.End();
 
              base.Draw(gameTime);
